6206b0aedab21b21f477794efa4c33bd1b1329ab
Anonymous Maarten <anonymous.maarten@gmail.com>
2018-02-22 00:23:20 +0100

    fix sdl2

--- conan_openrw_deps_1/sdl2.cpp
+++ conan_openrw_deps_1/sdl2.cpp
@@ -1,14 +1,17 @@
 #include <SDL.h>
 
-#include <cstdio>
+#include <iostream>
 
 int do_sdl2(int argc, char **argv) {
-    if (SDL_Init(SDL_INIT_EVENTS) < 0) {
-        puts("Failed to initialize SDL2 (events)!\n");
-        return 1;
-    }
+    SDL_version compiled;
+    SDL_version linked;
 
-    SDL_Quit();
+    SDL_VERSION(&compiled);
+    SDL_GetVersion(&linked);
+    std::cout << "We compiled against SDL version "
+        << int(compiled.major) << "." << int(compiled.minor) << "." << int(compiled.patch) << "\n";
+    std::cout << "But we are linking against SDL version "
+        << int(compiled.major) << "." << int(compiled.minor) << "." << int(compiled.patch) << "\n";
 
     return 0;
 }
