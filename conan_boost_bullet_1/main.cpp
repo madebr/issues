8acd2af90b0ead81fd9087f88941adc6faadef0a
Anonymous Maarten <anonymous.maarten@gmail.com>
2018-02-21 19:26:30 +0100

    Disabled some tests + test combo of boost and bullet

---
+++ conan_boost_bullet_1/main.cpp
@@ -0,0 +1,80 @@
+#include <boost/version.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+
+#include <BulletCollision/NarrowPhaseCollision/btManifoldPoint.h>
+#include <BulletCollision/NarrowPhaseCollision/btPersistentManifold.h>
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <string>
+
+bool contactProcessedCallback(btManifoldPoint &, void *, void *) {
+    return false;
+}
+
+int main(int argc, char **argv) {
+    gContactProcessedCallback = contactProcessedCallback;
+
+    std::cout << "boost version " << BOOST_LIB_VERSION << "\n";
+    const std::string s1 = "random text";
+    std::string s2;
+    s2.reserve(s1.size()+1);
+    std::transform(s1.cbegin(), s1.cend(), std::back_inserter(s2), [](unsigned char c) -> unsigned char {return ::toupper(c);});
+    std::cout << "iequals: ";
+    if (!boost::algorithm::iequals(s1, s2)) {
+        return 1;
+    }
+    std::cout << "success!\n";
+
+    auto path = boost::filesystem::initial_path();
+    std::cout << "initial path = " << path << "\n";
+    auto parent_path = path.parent_path();
+    std::cout << "parent = " << parent_path << "\n";
+
+    namespace po = boost::program_options;
+    po::options_description desc("conan_boost options");
+    desc.add_options()(
+        "value,v", po::value<size_t>()->value_name("VALUE"), "Set value");
+
+    boost::program_options::variables_map vm;
+    try {
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+    } catch (po::error &ex) {
+        std::cout << "Error parsing arguments: " << ex.what() << std::endl;
+    }
+
+    namespace pt = boost::property_tree;
+    pt::ptree tree;
+    const std::string ini_string =
+R"(
+[head1]
+key1=value1
+key2=value2
+[head2]
+key3=value3
+key4=value4
+)";
+    std::istringstream iss(ini_string);
+    pt::read_ini(iss, tree);
+
+    if (tree.get<std::string>("head1.key1").compare("value1")) {
+        return 1;
+    }
+    if (tree.get<std::string>("head1.key2").compare("value2")) {
+        return 1;
+    }
+    if (tree.get<std::string>("head2.key3").compare("value3")) {
+        return 1;
+    }
+    if (tree.get<std::string>("head2.key4").compare("value4")) {
+        return 1;
+    }
+    return 0;
+}
---
+++ conan_boost_bullet_1/run.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+from shutil import which, rmtree
+from subprocess import run, PIPE
+import os
+import sys
+
+srcdir = Path(__file__).absolute().parent
+bindir = srcdir / 'build'
+if bindir.is_dir():
+    rmtree(bindir)
+bindir.mkdir()
+
+cmake_generator = os.environ.get('CMAKE_GENERATOR', 'Unix Makefiles')
+configuration = 'Release'
+
+
+def desc():
+    print((srcdir / 'description.txt').open().read())
+    sys.stdout.flush()
+
+
+def init():
+    conan_exec = which('conan')
+
+    if not conan_exec:
+        pip_install = ['pip', 'install']
+        if sys.platform != 'win32':
+            pip_install  += ['--user']
+        pip_install += ['conan']
+        run(pip_install, check=True)
+        conan_exec = which('conan')
+
+    if not conan_exec:
+        print('Cannot find conan after installation!', file=sys.stderr)
+    run(['conan', 'user'], check=True)
+
+    remotes = run(['conan', 'remote', 'list'], stdout=PIPE, check=True)
+    if b'bincrafters' not in remotes.stdout:
+        run(['conan', 'remote', 'add', 'bincrafters', 'https://api.bintray.com/conan/bincrafters/public-conan'], check=True)
+
+
+def build():
+    run(['conan', 'install', str(srcdir), '-s', 'build_type={}'.format(configuration)], cwd=bindir, check=True)
+    run(['cmake', str(srcdir), '-DCMAKE_BUILD_TYPE={}'.format(configuration), '-G{}'.format(cmake_generator)], cwd=bindir, check=True)
+    run(['cmake', '--build', str(bindir), '--config', configuration], cwd=bindir, check=True)
+
+
+def test():
+    run(['ctest', '-C', configuration], cwd=bindir, check=True)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        sys.argv += ['desc', 'init', 'build', 'test']
+
+    if 'desc' in sys.argv:
+        desc()
+
+    if 'init' in sys.argv:
+        init()
+
+    if 'build' in sys.argv:
+        build()
+
+    if 'test' in sys.argv:
+        test()
