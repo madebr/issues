d49331277b5a656f9aa52b5003c5d02509083520
Anonymous Maarten <anonymous.maarten@gmail.com>
2018-02-23 16:31:26 +0100

    no cbegin and cend

--- conan_boost_algorithm_1/main.cpp
+++ conan_boost_algorithm_1/main.cpp
@@ -7,7 +7,7 @@ int main() {
     const std::string s1 = "random text";
     std::string s2;
     s2.reserve(s1.size()+1);
-    std::transform(s1.cbegin(), s1.cend(), std::back_inserter(s2), [](unsigned char c) -> unsigned char {return ::toupper(c);});
+    std::transform(s1.begin(), s1.end(), std::back_inserter(s2), [](unsigned char c) -> unsigned char {return ::toupper(c);});
     std::cout << "iequals: ";
     if (!boost::algorithm::iequals(s1, s2)) {
         return 1;
