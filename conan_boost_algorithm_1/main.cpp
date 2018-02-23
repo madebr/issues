7467d3c5dc790bd90583c3c383909eae7a39c596
Anonymous Maarten <anonymous.maarten@gmail.com>
2018-02-23 02:51:33 +0100

    Only having boost_algorithm as dependency

---
+++ conan_boost_algorithm_1/main.cpp
@@ -0,0 +1,16 @@
+#include <boost/algorithm/string/predicate.hpp>
+
+#include <iostream>
+#include <string>
+
+int main() {
+    const std::string s1 = "random text";
+    std::string s2;
+    s2.reserve(s1.size()+1);
+    std::transform(s1.cbegin(), s1.cend(), std::back_inserter(s2), [](unsigned char c) -> unsigned char {return ::toupper(c);});
+    std::cout << "iequals: ";
+    if (!boost::algorithm::iequals(s1, s2)) {
+        return 1;
+    }
+    return 0;
+}
