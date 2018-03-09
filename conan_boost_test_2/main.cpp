a863863e961e27210539031ae77598fe0532273a
Anonymous Maarten <anonymous.maarten@gmail.com>
2018-03-09 19:17:54 +0100

    alternative static boost test

--- conan_boost_test_2/main.cpp
+++ conan_boost_test_2/main.cpp
@@ -1,2 +1,9 @@
 #define BOOST_TEST_MODULE conan_boost_test_1
+#define BOOST_TEST_ALTERNATIVE_INIT_API
 #include <boost/test/unit_test.hpp>
+
+// entry point:
+int main(int argc, char* argv[], char* envp[])
+{
+  return boost::unit_test::unit_test_main(init_unit_test, argc, argv);
+}
