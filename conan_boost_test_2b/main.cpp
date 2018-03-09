d991ddeee3c72bb5542a290734c46cd66c102aa3
Anonymous Maarten <anonymous.maarten@gmail.com>
2018-03-09 19:43:09 +0100

    add conan_boost_test_2b

---
+++ conan_boost_test_2b/main.cpp
@@ -0,0 +1,6 @@
+#define BOOST_TEST_MODULE conan_boost_test_1
+#include <boost/test/unit_test.hpp>
+
+int cpp_main(int argc, char **argv) {
+    return 0;
+}
---
+++ conan_boost_test_2b/test_nop.cpp
@@ -0,0 +1,17 @@
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(NopTests)
+
+BOOST_AUTO_TEST_CASE(noptest_1) {
+    {
+        BOOST_CHECK(true);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(noptest_2) {
+    {
+        BOOST_CHECK(true);
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
