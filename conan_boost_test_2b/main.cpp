803c360ef14c7367c9993c39af3b12f15763e120
Anonymous Maarten <anonymous.maarten@gmail.com>
2018-03-09 19:48:53 +0100

    cpp_main(int, CONST char*)

--- conan_boost_test_2b/main.cpp
+++ conan_boost_test_2b/main.cpp
@@ -1,6 +1,6 @@
 #define BOOST_TEST_MODULE conan_boost_test_1
 #include <boost/test/unit_test.hpp>
 
-int cpp_main(int argc, char **argv) {
+int cpp_main(int argc, const char **argv) {
     return 0;
 }
